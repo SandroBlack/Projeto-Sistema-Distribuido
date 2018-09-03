082a1f04fb6e14c167c29dee32774ccbb9e127c3
weverson10 <weverson_acioly@hotmail.com>
2018-09-03 01:16:53 -0300

    Enviar informações do formário para uma página PHP

---
+++ dadosCadastro.php
@@ -0,0 +1,9 @@
+<?php 
+
+$Nome = $_GET["nomeCadastro"];
+$Email = $_GET["emailCadastro"];
+$Senha = $_GET["senhaCadastro"]; 
+
+echo "Nome: ".$Nome."<br/>E-mail: ".$Email."<br/>Senha: ".$Senha;
+
+?>
