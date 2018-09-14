a96c5717727149e66420630526f0c147ea6a4a8e
RicardoAndersonJC <32003271+RicardoAndersonJC@users.noreply.github.com>
2018-09-14 20:18:16 -0300

    Update dadosCadastro.php

--- dadosCadastro.php
+++ dadosCadastro.php
@@ -3,7 +3,8 @@
 $Nome = $_GET["nomeCadastro"];
 $Email = $_GET["emailCadastro"];
 $Senha = $_GET["senhaCadastro"]; 
-
-echo "Nome: ".$Nome."<br/>E-mail: ".$Email."<br/>Senha: ".$Senha;
+$SEXO = $_GET["SEXO"]
+echo "Nome: ".$Nome."<br/>E-mail: ".$Email."<br/>Senha: ".$Senha
+  "SEXO: " ".$SEXO.";
 
 ?>
