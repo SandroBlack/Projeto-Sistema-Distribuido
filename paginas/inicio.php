de90e7d14bf05ede45125dbba3491a667a28591a
SandroBlack <31974088+SandroBlack@users.noreply.github.com>
2018-10-01 11:34:35 -0300

    up
    att

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();		
-    if(!isset($_SESSION["logado"]) || $_SESSION["logado"] == true){      
+    if(!isset($_SESSION["logado"]) || $_SESSION["logado"] != true){      
         header("Location:../index.html");
     }
 ?>
